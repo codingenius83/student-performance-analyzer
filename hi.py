d84f9136dea967261590b7c7d6f6d64e30f0feb2
StudentPM <studentpm@Administrators-MacBook-Pro.local>
2026-09-18 15:36:29 -0700

    SUP

--- hi.py
+++ hi.py
@@ -12,8 +12,8 @@ grade_lvl = int(input('\nwhat grade lvl is the student in?(1-12)'))
 assignment_avg = input('\nwhat is the students assignment average?')
 quiz_avg = input('\nwhat is the students quiz average?')
 test_avg = input('\nwhat is the students test average?')
-attendance_percentage = input('\nwhat is the students attendance percentage?')
-missing_assignments = input('\nhow many missing assignments does the student have?')
+attendance_percentage = float(input('\nwhat is the students attendance percentage?'))
+missing_assignments = int(input('\nhow many missing assignments does the student have?'))
 def calculate_grade(assignment_average, quiz_average, test_average):
     assignments_w = 3/10
     quizzes_w = 3/10
@@ -39,10 +39,59 @@ def letter_grader(num_grade):
     return letter_grad
 letter_grade = letter_grader(number_grade)
 print(letter_grade)
-def attendance_status(attendance):
+def attendance_status_checker(attendance):
+    status = ''
     if attendance <= 0:
         attendance == 0
-    if attendance <<< ,
+    if attendance < 80:
+        status = 'poor attendance'
+    elif attendance >= 80 and attendance < 90:
+        status = 'attendance warning'
+    elif attendance >= 90 and attendance < 95:
+        status = 'good attendance'
+    else:
+        status = 'excellent attendance'
+    return status
+attendance_status = attendance_status_checker(attendance_percentage)
+print(attendance_status)
+def assignment_status_checker(missing_assigns):
+    status = ''
+    if missing_assigns <= 0:
+        missing_assigns = 0
+    if missing_assigns == 0:
+        status = 'Excellent'
+    elif missing_assigns == 1 or missing_assigns == 2:
+        status = 'good'
+    elif missing_assigns == 3 or missing_assigns == 4:
+        status = 'warning'
+    else:
+        status = 'critical'
+    return status
+assignment_status = assignment_status_checker(missing_assignments)
+print(assignment_status)
+def check_eligibility(overall_grade, attendance, assignments_missing):
+    academic_eligibility = ''
+    reason = ''
+    if overall_grade >= 70:
+        if attendance >= 90:
+            if assignments_missing <= 2:
+                academic_eligibility = 'ELIGIBLE'
+                reason = 'student passed all the requirements'
+            else:
+                academic_eligibility = 'NOT ELIGIBLE'
+                reason = 'too many missing assignments'
+        else:
+            academic_eligibility = 'NOT ELIGIBLE'
+            reason = 'attendance is too low'
+    else:
+        academic_eligibility = 'NOT ELIGIBLE'
+        reason = 'overall grade is too low'
+    return academic_eligibility, reason
+acadmic_eligibility, reason = check_eligibility(number_grade, attendance_percentage, missing_assignments)
+
+
+
+
         
         
 
